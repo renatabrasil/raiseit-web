739f1426dbbc8a3754c9e5b3ecfda14befd0f335
Eduardo <odraudelp@gmail.com>
2013-11-26 15:06:24 -0300

    @T94 Commit2 -

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,10 +3,10 @@ class Student < Individual
   #default_scope {where(:type => STUDENT)}
   scope :student, -> { where(type: 'Student') }
   
-  has_one :user, as: :user_account, dependent: :destroy
+  # has_one :user, as: :user_account, dependent: :destroy
   
   attr_accessible :enrollment, :user_attributes
 
-  accepts_nested_attributes_for :user, :allow_destroy => true
+  #accepts_nested_attributes_for :user, :allow_destroy => true
   
 end
