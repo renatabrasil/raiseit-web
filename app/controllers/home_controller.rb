b32092340d988c78169d5920608f15ee5128661d
renata <renata@renata-Inspiron-N4010>
2013-11-28 13:52:26 -0300

    @T100 Alerta de vencimento - 90%

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,9 @@
 class HomeController < ApplicationController
+  def index
+    date1 = DateTime.parse(Time.now.to_s)
+    format1 = "%Y-%m-%d"
+    str_date = date1.strftime(format1)
+    @size_expired_payments = Payment.count_by_sql " SELECT COUNT(*) FROM payments p WHERE p.expiration_date <= " + str_date.to_s + " and p.account_type like Enrollment "
+    
+  end
 end
