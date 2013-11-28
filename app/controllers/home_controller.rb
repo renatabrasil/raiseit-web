8847fee13ff975ceddb929fb8e523a89193d31ba
Renata Brasil <brasill.renata@gmail.com>
2013-11-28 16:32:03 -0300

    @T100 Alerta de vencimento - 98%.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,18 @@
 class HomeController < ApplicationController
   def index
-    date1 = DateTime.parse(Time.now.to_s)
-    format1 = "%Y-%m-%d"
-    str_date = date1.strftime(format1)
-    @size_expired_payments = Payment.count_by_sql " SELECT COUNT(*) FROM payments p WHERE p.expiration_date <= " + str_date.to_s + " and p.account_type like Enrollment "
+
+    sql = "SELECT COUNT(*) FROM payments p WHERE p.expiration_date <= " +
+    "CURRENT_TIMESTAMP AND p.account_type LIKE 'Enrollment' AND " +
+    " p.paid = 'false' "
     
+    # Pro aplicativo
+    # if user_signed_in?
+      # if current_user.id == User::ADMIN_ROOT_ID
+        # sql + "AND p.individual_id = " + current_user.id.to_s
+      # end
+    # end
+
+    @size_expired_payments = Payment.count_by_sql sql
+
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  
+  ADMIN_ROOT_ID = 1
+  
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
