b6ac7620273cd4af9f5ef5a6982282a0da87a6a6
Eduardo <odraudelp@gmail.com>
2014-01-20 16:20:11 -0300

    @T153 Permitido o acesso aos atributos :active e :modality_id que estavam bloqueados. Adicionado a listagem de alunos os campos de modalidade e active.

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -7,7 +7,7 @@ class Enrollment < ActiveRecord::Base
 
   has_many :payments, as: :account, dependent: :destroy
 
-  attr_accessible :start_date, :note, :value, :registration_fee, :discount, :student_id
+  attr_accessible :start_date, :note, :value, :registration_fee, :discount, :student_id, :active, :modality_id
   def expired_payment
     return Payment.where(['individual_id = ? AND paid = ? AND expiration_date <= ?', self.student_id, false, Date.today])
   end
