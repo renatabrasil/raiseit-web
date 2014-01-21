7eb882d6d0fc876eba8b2b5d08cc7a4eb3a6f5d1
Eduardo <odraudelp@gmail.com>
2014-01-21 15:47:19 -0300

    @T153 Criado filtros também para o enrollment. Adcionado o filtro para exibir se o aluno é ativo ou não. Finalizada as tarefas iniciais da histrória US-83

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,7 +1,10 @@
 class EnrollmentsController < ApplicationController
   def index
-    @enrollments = Enrollment.all
+    @search = Enrollment.joins(:student).search(params[:q])
+    @enrollments = @search.result(distinct: true)
+    
+    # @enrollments = Enrollment.all
     # @vencimentos = Payment.where({ name: "Joe", email: "joe@example.com" })
-    #@vencimentos = Payment.where(individual_id: author)
+    # @vencimentos = Payment.where(individual_id: author)
   end
 end
