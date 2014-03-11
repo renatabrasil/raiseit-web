6993cf619663c067d5ba6be6f9e39425fa061173
Renata Brasil <brasill.renata@gmail.com>
2014-03-11 16:16:20 -0300

    @T202 Atualizar o CRUD de turmas, pois agora uma turma pode ter mais de um professor. Verificar o impacto nos demais casos de uso.

--- app/models/class_gym.rb
+++ app/models/class_gym.rb
@@ -4,7 +4,10 @@ class ClassGym < ActiveRecord::Base
   
   has_and_belongs_to_many :students
   has_and_belongs_to_many :instructors, :class_name => 'Employee', 
-    :association_foreign_key => 'instructor_id', :join_table => 'class_gyms_instructors'
+    :association_foreign_key => 'instructor_id', 
+    :join_table => 'class_gyms_instructors', :uniq => true
+    
+  validates :instructors, :modality, presence: true
     
   attr_accessible :schedule, :capacity, :modality_id, :instructor_ids, :instructors, 
     :student_ids, :open, :start_date, :instructors_attributes
@@ -18,4 +21,5 @@ class ClassGym < ActiveRecord::Base
     end
     return self.start_date.blank? ? 0 : (self.start_date.to_date > today ? 0 : (today - self.start_date.to_date).to_i)
   end
+  
 end
