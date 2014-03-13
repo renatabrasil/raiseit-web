d27c570481547d5a420bab7920189a623be1a13e
Renata Brasil <brasill.renata@gmail.com>
2014-03-13 16:32:48 -0300

    @T211 Refatorar ClassGym. Substituir por GymClass. No model, controller, views e tabelas no banco.

--- test/controllers/class_gyms/add_students_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ClassGyms::AddStudentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/helpers/class_gyms/add_students_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class ClassGyms::AddStudentsHelperTest < ActionView::TestCase
-end
